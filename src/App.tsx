3e9e4e018a9a20ef4933e0ff2d00f4a8483dd9a2
Yashkokane <37013378+Yashkokane@users.noreply.github.com>
2023-06-22 11:43:58 +0100

    Build Responsive layout with Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, //1024px
+        }}>
+        <GridItem area={"nav"}>
+          <Navbar></Navbar>
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"gold"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area={"main"} bg={"skyblue"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
