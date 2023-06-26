3563624200bbdba6e9f110369e846a5a716acb1f
Yashkokane <37013378+Yashkokane@users.noreply.github.com>
2023-06-26 15:13:34 +0100

    display score and optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import { Game } from "../Hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImgUrl from "../Services/Image-Url";
 
 interface Props {
   game: Game;
@@ -11,13 +12,16 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card maxW="sm" borderRadius={10} overflow={"hidden"}>
-        <Image src={game.background_image}></Image>
+        <Image src={getCroppedImgUrl(game.background_image ) }></Image>
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <PlatformIconList
-            platforms={game.parent_platforms.map(
-              (p) => p.platform
-            )}></PlatformIconList>
+          <HStack justifyContent={"space-between"} paddingTop={"5px"}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map(
+                (p) => p.platform
+              )}></PlatformIconList>
+            <CriticScore score={game.metacritic}></CriticScore>
+          </HStack>
         </CardBody>
       </Card>
     </>
