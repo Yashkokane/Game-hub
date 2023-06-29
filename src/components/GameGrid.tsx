3c67d914104a83f2351408441298e4e9247cd4f8
Yashkokane <37013378+Yashkokane@users.noreply.github.com>
2023-06-29 23:57:11 +0100

    changed themes and refactored game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,27 +12,24 @@ interface Props {
 const GameGrid = ({ GameQuery }: Props) => {
   const { Data, error, isLoading } = useGames(GameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}>
-        {isLoading &&
-          Skeletons.map((Skeleton) => (
-            <GameCardContainer key={Skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {Data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}>
+      {isLoading &&
+        Skeletons.map((Skeleton) => (
+          <GameCardContainer key={Skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {Data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
