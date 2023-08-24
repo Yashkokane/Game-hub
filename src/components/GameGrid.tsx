949296077090d9117439ee147e73fc6e8d24bd14
Yashkokane <37013378+Yashkokane@users.noreply.github.com>
2023-08-24 12:06:09 +0100

    infinite scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { GameQuery } from "../App";
 import useGames from "../Hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import React from "react";
 
 interface Props {
   GameQuery: GameQuery;
@@ -21,8 +22,38 @@ const GameGrid = ({ GameQuery }: Props) => {
   } = useGames(GameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   if (error) return <Text>{error.message}</Text>;
+
+  const fetchGameCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
   return (
-    <Box padding="10px">
+    <InfiniteScroll
+      dataLength={fetchGameCount}
+      hasMore={!!hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner />}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        padding="10px">
+        {isLoading &&
+          Skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
+        ))}
+      </SimpleGrid>
+    </InfiniteScroll>
+
+    /*<Box padding="10px">
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
         {isLoading &&
           Skeletons.map((skeleton) => (
@@ -45,7 +76,7 @@ const GameGrid = ({ GameQuery }: Props) => {
           {isFetchingNextPage ? "Loading..." : "Load More"}
         </Button>
       )}
-    </Box>
+    </Box>*/
   );
 };
 
